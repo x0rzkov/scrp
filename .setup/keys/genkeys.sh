292c4b0db4d2b8c2458e3e72a0c5ca9e63358ecf
Andrew Grosser <dioptre@gmail.com>
2019-12-19 17:38:09 -0800

    updated keys

--- .setup/keys/genkeys.sh
+++ .setup/keys/genkeys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-openssl req -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout backend.key -days 99999999 -out backend.cert -subj '/CN=backend.local'
-openssl req -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout frontend.key -days 99999999 -out frontend.cert -subj '/CN=frontend.local'
+openssl req -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout backend.key -days 9999 -out backend.cert -subj '/CN=backend.local'
+openssl req -new -x509 -sha256 -newkey rsa:2048 -nodes -keyout frontend.key -days 9999 -out frontend.cert -subj '/CN=frontend.local'
